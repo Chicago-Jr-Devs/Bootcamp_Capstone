91eb323af4266f6fc9935f8278324a4c70d5d8bf
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2021-03-11 14:08:47 -0600

    functioning build of Passport, added middleware packs

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session")
+const morgan = require("morgan")
+const helmet = require('helmet')
+// Requiring passport as we've configured it
 const passport = require("./config/passport");
 const routes = require("./routes");
 
+
+// Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const SYNC_OPTIONS = {
   force: process.env.NODE_ENV === "test",
 };
 
 const db = require("./models");
-
+// Creating express app and configuring middleware needed for authentication
 const app = express();
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -38,15 +49,15 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(morgan('tiny'));
 
 
 
 
-
-
+// Requiring our routes
 app.use(routes);
-
-db.sequelize.sync({ force: true }).then(() => {
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync(SYNC_OPTIONS).then(() => {
   app.listen(PORT, () => {
     console.log(
       `==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`
