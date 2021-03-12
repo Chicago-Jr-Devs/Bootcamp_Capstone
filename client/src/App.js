c59a7706dbd8033c72a7f29653700834364f7953
Latifah Lillie <queen1892@gmail.com>
2021-03-11 19:26:47 -0600

    adding consent, login, register, home pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import Consent from "./pages/Consent";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import React from "react";
+import ReactDOM from "react-dom";
+
 import './App.css';
+
+
 //import Home from './pages/Home'
 function App() {
+
+  // ReactDOM.render(<App />, document.getElementById("root"));
   return (
     <div>
       <p>Hello World</p>
+      <Consent />
+      <Home />
+      <Register />
+      <Login />
     </div>
   );
 }
