8ce041c80c24d5aef9c7bc82b3ac7fd4120b395b
Clem <valles.clement@gmail.com>
2021-03-12 17:30:16 -0600

    Adding the wall structure and components: Header, search bar and card

---
+++ client/src/components/MainWall/HeaderWall.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+// Import material UI components
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    }
+}));
+
+export function HeaderWall(props){
+    const classes = useStyles();
+    
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={4}>
+                <Grid item sm={12} align="center">
+                    <div>
+                        <h1>Soapbox</h1>
+                        <p>Raised platform for your community</p>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
