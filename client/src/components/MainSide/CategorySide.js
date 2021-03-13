2ad34b2a8f9d3c32acc2cdbc8306528fd37f7dc9
Clem <valles.clement@gmail.com>
2021-03-13 10:10:36 -0600

    Adding some icons on the category and correcting the home path

--- client/src/components/MainSide/CategorySide.js
+++ client/src/components/MainSide/CategorySide.js
@@ -3,10 +3,20 @@ import React from "react"
 // Import material UI components
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import GavelIcon from '@material-ui/icons/Gavel';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import CommuteIcon from '@material-ui/icons/Commute';
+import NaturePeopleIcon from '@material-ui/icons/NaturePeople';
+import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
+import EcoIcon from '@material-ui/icons/Eco';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
+        width: '100%',
+        maxWidth: 360,
     }
 }));
 
@@ -20,14 +30,14 @@ export function CategorySide(props){
                     <div>
                         <h3>Categories:</h3>
                         <ul>
-                            <li><a href="">Education</a></li>
-                            <li><a href="">Law Enforcement</a></li>
-                            <li><a href="">Infrastructure</a></li>
-                            <li><a href="">Transportation</a></li>
-                            <li><a href="">Environment</a></li>
-                            <li><a href="">Public Spaces</a></li>
-                            <li><a href="">Public Policy</a></li>
-                            <li><a href="">Others</a></li>
+                            <li><a href=""><MenuBookIcon /> Education</a></li>
+                            <li><a href=""><GavelIcon /> Law Enforcement</a></li>
+                            <li><a href=""><EmojiTransportationIcon /> Infrastructure</a></li>
+                            <li><a href=""><CommuteIcon /> Transportation</a></li>
+                            <li><a href=""><EcoIcon /> Environment</a></li>
+                            <li><a href=""><NaturePeopleIcon /> Public Spaces</a></li>
+                            <li><a href=""><AccountBalanceIcon /> Public Policy</a></li>
+                            <li><a href=""><AnnouncementIcon /> Others</a></li>
                         </ul>
                     </div>
                 </Grid>
