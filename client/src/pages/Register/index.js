bd029e83d117dcbbf183a4a00b11f3f3f00ca933
Clem <valles.clement@gmail.com>
2021-03-13 14:00:33 -0600

    WIP taking data from register and trying to post it

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,15 +1,33 @@
-import './Register.css';
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
 import { Header, Input, RegisterBtn } from "../../components/Register"
+import './Register.css';
 
 export default function Register(){
-    return (
+    const [user, setUser] = useState({email:'', password:'', firstName:'', lastName:'', zipCode:''});
+    const [users, setUsers] = useState([]);
+
+    const handleData = (event) => {
+        const { id, value } = event.target
+        setUser({...user, [id]: value})
+    }
+
+    async function submitUser(event) {
+        event.preventDefault()
+        console.log("hi", user)
+        await axios.post('/users', user)
         
+        setUsers([...users, user])
+        setUser({email:'', password:'', firstName:'', lastName:'', zipCode:''})
+    }
+
+    useEffect(() => console.log("user", users), [users]);
+
+    return (
         <div>
             <Header />
-            <Input />
-            <RegisterBtn />
-
+            <Input handleData={handleData}/>
+            <RegisterBtn submitUser={submitUser}/>
          </div>
-        
     )
 };
