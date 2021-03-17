040b031e976cf729b359f78f9e76688ca01cb704
Peter O'Connor <ftr.cafekundera@yahoo.com>
2021-03-16 23:03:54 -0400

    WIP, working on adding an AJAX call

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //Import react pages 
 import Consent from "./pages/Consent";
@@ -11,12 +11,30 @@ import './App.css';
 // ReactDOM.render(<App />, document.getElementById("root"));
 
 export default function App() {
+  const [address, setAddress] = useState ([])
+  const getAddressInfo = async () => {
+    try {
+      const searchString = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=20007&key=AIzaSyCdgqZ9mJc1Qxkg4JcgRLX1d2_ToFGq1hU`
+      const response = await fetch(searchString)
+      const myStuff = await response.json()
+      setAddress(myStuff.main)
+    } 
+      catch (err){
+    }
+  }
 
+  const handleSearch = (event) => {
+    event.preventDefault();
+    getAddressInfo();
+}
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/">
+          <form onSubmit={handleSearch}>
+          <input type="submit" value="Search"/>
+          </form>
             <Login />
           </Route>
           <Route path="/register">
