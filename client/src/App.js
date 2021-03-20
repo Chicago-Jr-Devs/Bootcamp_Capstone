da9bae63db42bb8ebf540b299c53073b7f3c7eff
Peter O'Connor <ftr.cafekundera@yahoo.com>
2021-03-20 11:09:12 -0400

    Making the AJAX call to retrieve ward information

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 //Import style sheet 
 import './App.css';
+import user from "../../models/user";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -14,13 +15,26 @@ export default function App() {
   const [address, setAddress] = useState ([])
   const getAddressInfo = async () => {
     try {
-      const searchString = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=20007&key=AIzaSyCdgqZ9mJc1Qxkg4JcgRLX1d2_ToFGq1hU`
+      const zipcode = $(user.zipcode).val().trim()
+      const searchString = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${zipcode}&key=AIzaSyCdgqZ9mJc1Qxkg4JcgRLX1d2_ToFGq1hU`
       const response = await fetch(searchString)
       const myStuff = await response.json()
       setAddress(myStuff.main)
+
+      $("#submitButton").on("click", function(){
+      $("#ward").html("")
+        console.log(zipcode)
+        
+        let address = zipcode
+        callAddress(address, ward)
+      })
     } 
       catch (err){
     }
+  }
+
+  function displayWard() {
+
   }
 
   const handleSearch = (event) => {
