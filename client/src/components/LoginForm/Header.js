cb44a84cf2df65f2a4130159a52c8b810b1f0585
Peter O'Connor <71603259+ftrcafekundera@users.noreply.github.com>
2021-03-20 16:52:06 -0400

    Copyedited Header.js

--- client/src/components/LoginForm/Header.js
+++ client/src/components/LoginForm/Header.js
@@ -4,7 +4,7 @@ export function Header(props) {
     return (
        <div>
            <h1>SoapBox</h1>
-           <p>Please Sign In or Register to Access the SoapBox</p>
+           <p>Please Sign In or Register to Access SoapBox</p>
        </div>
     )
 }
