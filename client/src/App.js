d9d7c3b4dfe22cc60bea579dd3a47bb4a48df3ec
Clem <valles.clement@gmail.com>
2021-03-20 11:44:54 -0500

    Fixing error form App.js with user request API

--- client/src/App.js
+++ client/src/App.js
@@ -7,48 +7,48 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 //Import style sheet 
 import './App.css';
-import user from "../../models/user";
+// import user from "../../models/user";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
 export default function App() {
-  const [address, setAddress] = useState ([])
-  const getAddressInfo = async () => {
-    try {
-      const zipcode = $(user.zipcode).val().trim()
-      const searchString = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${zipcode}&key=AIzaSyCdgqZ9mJc1Qxkg4JcgRLX1d2_ToFGq1hU`
-      const response = await fetch(searchString)
-      const myStuff = await response.json()
-      setAddress(myStuff.main)
+//   const [address, setAddress] = useState ([])
+//   const getAddressInfo = async () => {
+//     try {
+//       const zipcode = $(user.zipcode).val().trim()
+//       const searchString = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${zipcode}&key=AIzaSyCdgqZ9mJc1Qxkg4JcgRLX1d2_ToFGq1hU`
+//       const response = await fetch(searchString)
+//       const myStuff = await response.json()
+//       setAddress(myStuff.main)
 
-      $("#submitButton").on("click", function(){
-      $("#ward").html("")
-        console.log(zipcode)
+//       $("#submitButton").on("click", function(){
+//       $("#ward").html("")
+//         console.log(zipcode)
         
-        let address = zipcode
-        callAddress(address, ward)
-      })
-    } 
-      catch (err){
-    }
-  }
+//         let address = zipcode
+//         callAddress(address, ward)
+//       })
+//     } 
+//       catch (err){
+//     }
+//   }
 
-  function displayWard() {
+//   function displayWard() {
 
-  }
+//   }
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    getAddressInfo();
-}
+//   const handleSearch = (event) => {
+//     event.preventDefault();
+//     getAddressInfo();
+// }
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/">
-          <form onSubmit={handleSearch}>
+          {/* <form onSubmit={handleSearch}>
           <input type="submit" value="Search"/>
-          </form>
+          </form> */}
             <Login />
           </Route>
           <Route path="/register">
